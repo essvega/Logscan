ebba59323048ea63c6ce9165bc352934a2660238
Ess Vega <34743796+essvega@users.noreply.github.com>
2019-12-20 16:42:36 -0500

    Add files via upload

--- read.py
+++ read.py
@@ -1,8 +1,24 @@
 import re
-list = []
+list = ["0"]
+print("Starting ... \nPlease do not close this window until we finish")
+
+
 
 with open('log.txt') as f:
     for line in f:
-        ip = re.findall( r'[1-2]{0,1}[0-6][8]{0,1}[0-9]{1}\.[1-2]{0,1}[0-9]{0,1}[0-9]{1}\.[1-2]{0,1}[0-9]{0,1}[0-9]{1}\.[1-2]{0,1}[0-9]{0,1}[0-9]{1}', line)
+        ip = re.findall( r'[1-2]{0,1}[0-9]{0,1}[0-9]{1}\.[1-2]{0,1}[0-9]{0,1}[0-9]{1}\.[1-2]{0,1}[0-9]{0,1}[0-9]{1}\.[1-2]{0,1}[0-9]{0,1}[0-9]{1}', line)
         if len(ip) > 0 :
-            print(ip)
+            for a in list:
+                if not ip in list:
+                    list.append(ip)
+
+for i in list:
+    print(i)
+print("                         ")
+print("                         ")
+print("                         ")
+print("*************************")
+print("*                       *")
+print("* Powered by @Essiel V. *")
+print("*                       *")
+print("*************************")
