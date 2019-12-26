6469caedc4c29854258af0717532ddf79d4e6c17
Ess Vega <34743796+essvega@users.noreply.github.com>
2019-12-26 14:15:17 -0500

    Add files via upload

--- read.py
+++ read.py
@@ -13,11 +13,6 @@ orderedlist = []
 latest = []
 print("Starting ... \nPlease do not close this window until we finish")
 
-def jprint(obj):
-    #Create a formatted string of the Python JSON object
-    text = json.dumps(obj, sort_keys=True, indent=4)
-    print(text)
-
 
 def organize():
     for j in list:
