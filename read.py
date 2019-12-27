0ead0e08edb18cc1fe5dcaceb4e25896869475cf
Ess Vega <34743796+essvega@users.noreply.github.com>
2019-12-27 08:43:46 -0500

    Adding Comments
    Adding Comments

--- read.py
+++ read.py
@@ -5,6 +5,7 @@ import json
 import ipinfo
 from IPy import IP
 
+#Request token for every indivual that tries to use this script. They must use their own token code
 access_token = input('Enter your token access:  ')
 handler = ipinfo.getHandler(access_token)
 
@@ -13,17 +14,20 @@ orderedlist = []
 latest = []
 print("Starting ... \nPlease do not close this window until we finish")
 
-
+#It saves all IP addresses found while running openfor()
 def organize():
     for j in list:
         for k in j:
             orderedlist.append(k)
 
+#It reduces the table to unique values. It will remove any IP address repeated
 def removing():
     for z in orderedlist:
         if z not in latest:
             latest.append(z)
           
+#From the previous list holding only unique values, this function will 
+#remove all IP addresses that are not recognized as PUBLIC
 def letsdoit():    
     for i in latest:
         ipe = IP(i)
@@ -35,6 +39,7 @@ def letsdoit():
                companies = "--NO DATA--"
             print("IP Address=> " + i + "; Country=> " + details.country_name + "; Region=> " + details.region + "; Company=> " + companies)
 
+#It opens every .txt file targeted
 def openfor():
     with open(dirname + '.txt') as f:
         for line in f:
@@ -44,6 +49,7 @@ def openfor():
                     if not ip in list:
                         list.append(ip)
 
+#It opens all .txt files concentrated in the same folder
 dir_path = os.path.dirname(os.path.realpath(__file__))
 for file in os.listdir(dir_path):
     if file.endswith(".txt"):
